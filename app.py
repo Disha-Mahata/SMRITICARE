a261f082843c45282cd56f99819f129ff84a574d
Disha-Mahata <dishamahata2009@gmail.com>
2026-01-20 20:12:01 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("🧠 SmritiCare")
+
+st.header("Caregiver")
+st.text_area("Add observation")
+
+st.header("Patient Engagement")
+st.button("Memory Quiz")
+st.button("Familiar Faces")
+
+st.header("Doctor Analyzer")
+st.write("Behavior Trend")
+st.write("Medication Adherence")
